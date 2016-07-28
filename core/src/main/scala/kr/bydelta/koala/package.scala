c949efef07578c4fe25b4a7e96ccec206755311d
bydelta <cd4209@gmail.com>
2016-07-28 09:59:29 +0900

    Travis support

--- core/src/main/scala/kr/bydelta/koala/package.scala
+++ core/src/main/scala/kr/bydelta/koala/package.scala
@@ -252,7 +252,7 @@ package object koala {
           case "부사어" | "이유" => FunctionalTag.Adjunct
           case "보어" | "인용" => FunctionalTag.Complement
           case "수식" | "명사구" => FunctionalTag.Modifier
-          case "동일" "보조 연결" | "의존 연결" | "대등 연결" | "체언 연결" => FunctionalTag.Conjunctive
+          case "동일" | "보조 연결" | "의존 연결" | "대등 연결" | "체언 연결" => FunctionalTag.Conjunctive
           case _ =>
             FunctionalTag.Undefined
         }
