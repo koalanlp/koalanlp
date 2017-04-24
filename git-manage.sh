17efaf9543e56a4f90bbc5241e8b63568aca88f5
nearbydelta <cd4209@gmail.com>
2017-04-24 20:37:00 +0900

    RELEASE v1.5.2 - Fix for issue #16 - More concrete test specs - Automatic snapshot builds

--- git-manage.sh
+++ git-manage.sh
@@ -7,32 +7,40 @@ TAG=`cat build.sbt | grep "val VERSION" | cut -d\" -f2`
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 
-if [[ $MSG == *"RELEASE"* ]]; then
-    # SET RELEASE TAG
-    git tag v$TAG
-    git push --tags
+if [[ $TRAVIS_EVENT_TYPE == "cron" ]]; then
+    cp sonatype.sbt ~/.sbt/0.13/
 
-    # GENERATE DOC
-    sbt ++$1 unidoc
+    # ADD SNAPSHOT VARIABLE
+    cat build.sbt | sed -e 's/val VERSION\s*=\s*"\(.*\)"/val VERSION = "\1-SNAPSHOT"/g' | tee build.sbt > \dev\null
+    sbt ++$TRAVIS_SCALA_VERSION publish
 
-    # CLONE GH-PAGES
-    cd $HOME
-    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/nearbydelta/KoalaNLP gh-pages > /dev/null
+elif [[ $TRAVIS_EVENT_TYPE == "push" ]]; then
+    if [[ $MSG == *"RELEASE"* ]]; then
+        # SET RELEASE TAG
+        git tag v$TAG
+        git push --tags
 
-    # COPY & PUSH
-    cd gh-pages
-    git rm -rf ./api/*
-    mkdir ./api
 
-    mv $WD/target/scala-$VER/unidoc $HOME/gh-pages/api/scala
-    mv $WD/target/javaunidoc $HOME/gh-pages/api/java
-    mv $WD/README.md $HOME/gh-pages/index.md
+        # GENERATE DOC
+        sbt ++$1 unidoc
 
-    git add -f ./api
-    git add -f ./index.md
+        # CLONE GH-PAGES
+        cd $HOME
+        git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/nearbydelta/KoalaNLP gh-pages > /dev/null
 
-    git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-    git push -fq origin gh-pages > /dev/null
-elif [[ $MSG == *"SNAPSHOT"* ]]; then
-    sbt ++$1 publish
+        # COPY & PUSH
+        cd gh-pages
+        git rm -rf ./api/*
+        mkdir ./api
+
+        mv $WD/target/scala-$VER/unidoc $HOME/gh-pages/api/scala
+        mv $WD/target/javaunidoc $HOME/gh-pages/api/java
+        mv $WD/README.md $HOME/gh-pages/index.md
+
+        git add -f ./api
+        git add -f ./index.md
+
+        git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+        git push -fq origin gh-pages > /dev/null
+    fi
 fi
