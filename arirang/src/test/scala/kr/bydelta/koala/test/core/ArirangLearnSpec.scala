423132315fcd45fdcae1337b989440934ffb0210
nearbydelta <cd4209@gmail.com>
2017-08-19 23:36:52 +0900

    Implement 'baseEntriesOf()' of Arirang Dictionary

---
+++ arirang/src/test/scala/kr/bydelta/koala/test/core/ArirangLearnSpec.scala
@@ -0,0 +1,52 @@
+package kr.bydelta.koala.test.core
+
+import kr.bydelta.koala.arirang.{Dictionary, Tagger}
+import org.apache.lucene.analysis.ko.morph.AnalysisOutput
+import org.specs2.execute.Result
+import org.specs2.mutable.Specification
+
+import scala.collection.JavaConverters._
+
+/**
+  * Created by bydelta on 16. 7. 30.
+  */
+class ArirangLearnSpec extends Specification with BasicWordLearnerSpecs[Seq[AnalysisOutput]] {
+  sequential
+
+  def getTagger = new Tagger
+
+  def getDict = Dictionary
+
+  "BasicWordLearner" should {
+    var lv0Empty = false
+
+    "extract all nouns" in {
+      val level0 = learner.extractNouns(text.toIterator, minOccurrence = 2, minVariations = 2)
+      val level2 = learner.extractNouns(text.toIterator, minOccurrence = 5, minVariations = 3)
+
+      lv0Empty = level0.isEmpty
+      level0.size must be_>=(level2.size)
+      level0 must not(containAnyOf(EXCLUDED_SET))
+      level0 must containAllOf(INC_1._1)
+      level0 must not(containAnyOf(INC_1._2))
+      level2 must containAllOf(INC_2._1)
+      level2 must not(containAnyOf(INC_2._2))
+    }
+
+    "learn all nouns" in {
+      Result.unit {
+        if (!lv0Empty) {
+          val tagger1 = getTagger
+          val beforeLearn = text.map(s => tagger1.tagSentence(s).singleLineString).mkString("\n")
+
+          learner.jLearn(text.toIterator.asJava, minOccurrence = 1, minVariations = 1)
+
+          val tagger2 = getTagger
+          val afterLearn = text.map(s => tagger2.tagSentence(s).singleLineString).mkString("\n")
+
+          beforeLearn must_!= afterLearn
+        }
+      }
+    }
+  }
+}
--- kryo/src/test/scala/kr/bydelta/koala/test/core/DictionaryImportSpec.scala
+++ kryo/src/test/scala/kr/bydelta/koala/test/core/DictionaryImportSpec.scala
@@ -36,10 +36,8 @@ object DictionaryImportSpec extends Specification {
       Result.unit {
         dictionaries.combinations(2).foreach {
           set =>
-            if (set.last != ADict)
-              Try(set.head.importFrom(set.last, _ == POS.NNP, fastAppend = true)) must beSuccessfulTry
-            if (set.head != ADict)
-              Try(set.last.importFrom(set.head, _ == POS.NNP, fastAppend = true)) must beSuccessfulTry
+            Try(set.head.importFrom(set.last, _ == POS.NNP, fastAppend = true)) must beSuccessfulTry
+            Try(set.last.importFrom(set.head, _ == POS.NNP, fastAppend = true)) must beSuccessfulTry
         }
       }
     }
