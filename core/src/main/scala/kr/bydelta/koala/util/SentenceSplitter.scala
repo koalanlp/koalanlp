bc7226610f1b7b8ba673817335cab1e3918c0e82
nearbydelta <cd4209@gmail.com>
2018-01-24 00:31:38 +0900

    Sentence Splitter Enhancement - Compute open/close parenthesis in char-level. (Previously, word-level)

--- core/src/main/scala/kr/bydelta/koala/util/SentenceSplitter.scala
+++ core/src/main/scala/kr/bydelta/koala/util/SentenceSplitter.scala
@@ -1,18 +1,14 @@
 package kr.bydelta.koala.util
 
 import kr.bydelta.koala.POS
-import kr.bydelta.koala.data.{Sentence, Word}
+import kr.bydelta.koala.data.{Morpheme, Sentence, Word}
 
 import scala.annotation.tailrec
-import scala.collection.mutable.ArrayBuffer
 
 object SentenceSplitter {
-  private val quoteRegex = "(?U)[\'\"]{1}".r
-  private val openParenRegex = "(?U)[\\(\\[\\{<〔〈《「『【‘“]{1}".r
-  private val closeParenRegex = "(?U)[\\)\\]\\}>〕〉》」』】’”]{1}".r
-  private val matchRegex = ("(?U)(\'[^\']*\'|\"[^\"]*\"|\\([^\\(\\)]*\\)|\\[[^\\[\\]]*\\]|\\{[^\\{\\}]*\\}|" +
-    "<[^<>]*>|〔[^〔〕]*〕|〈[^〈〉]*〉|《[^《》]*》|「[^「」]*」|『[^『』]*』|【[^【】]*】|‘[^‘’]*’|“[^“”]*”)").r
-  private val filterRegex = "(?U)[^\'\"\\(\\[\\{<〔〈《「『【‘“\\)\\]\\}>〕〉》」』】’”]+".r
+  private val quoteRegex = "\'\"＇＂"
+  private val openParenRegex = "([{‘“（［｛<〔〈《「『【"
+  private val closeParenRegex = ")]}’”）］｝>〕〉》」』】"
 
   /**
     * 분석결과를 토대로 문장을 분리함.
@@ -22,74 +18,65 @@ object SentenceSplitter {
     */
   def apply(para: Seq[Word]): Seq[Sentence] = split(para)
 
+  @tailrec
+  private def rollupParenStack(morphemes: Seq[Morpheme],
+                               stack: List[Char],
+                               isPreviousSL: Boolean = false,
+                               hasEndOfSentence: Boolean = false): (List[Char], Boolean) =
+    morphemes match {
+      case Seq() =>
+        (stack, if (stack.isEmpty) hasEndOfSentence else false)
+      case Seq(Morpheme(morph, POS.SF), tail@_*) if morph != "." || !isPreviousSL =>
+        // 영문 약어 뒤에 따라붙는 마침표가 아니면.
+        rollupParenStack(tail, stack, hasEndOfSentence = true)
+      case Seq(Morpheme(_, POS.SN), tail@_*) =>
+        // 숫자가 나타나면 종결부호 플래그 무시.
+        rollupParenStack(tail, stack)
+      case Seq(Morpheme(x, tag), tail@_*) =>
+        val newStack = x.foldLeft(stack) {
+          case (list, ch) if openParenRegex.contains(ch) =>
+            ch +: list
+          case (list, ch) if closeParenRegex.contains(ch) =>
+            list.headOption match {
+              case Some(stackHead) =>
+                if (closeParenRegex.indexOf(ch) == openParenRegex.indexOf(stackHead))
+                  list.tail
+                else list
+              case _ => list
+            }
+          case (list, ch) if quoteRegex.contains(ch) =>
+            list.headOption match {
+              case Some(stackHead) if stackHead == ch => list.tail
+              case _ => ch +: list
+            }
+          case (list, _) => list
+        }
+
+        rollupParenStack(tail, newStack,
+          isPreviousSL = tag == POS.SL,
+          hasEndOfSentence = hasEndOfSentence)
+    }
+
   /**
     * 분석결과를 토대로 문장을 분리함.
     *
-    * @param para 분리할 문단.
-    * @param pos  현재 읽고있는 위치.
-    * @param open 현재까지 열려있는 묶음기호 Stack.
-    * @param acc  현재까지 분리된 문장들.
+    * @param para       분리할 문단.
+    * @param parenStack 현재까지 열려있는 묶음기호 Stack.
+    * @param acc        현재까지 분리된 문장들.
     * @return 문장단위로 분리된 결과
     */
   @tailrec
   private def split(para: Seq[Word],
-                           pos: Int = 0,
-                           open: List[Char] = List(),
-                           acc: ArrayBuffer[Sentence] = ArrayBuffer()): Seq[Sentence] =
-  if (para.isEmpty) acc
+                    parenStack: List[Char] = List(),
+                    acc: Seq[Seq[Word]] = Seq(Seq.empty)): Seq[Sentence] =
+    if (para.isEmpty) acc.filter(_.nonEmpty).map(x => Sentence(x.reverse)).reverse
   else {
-    import kr.bydelta.koala.Implicit._
-    val rawEndmark = para.indexWhere(_.exists(POS.SF), pos)
-    val rawParen = para.indexWhere({
-      e =>
-        (e.exists(POS.SS) ||
-          openParenRegex.findFirstMatchIn(e.surface).isDefined ||
-          closeParenRegex.findFirstMatchIn(e.surface).isDefined ||
-          quoteRegex.findFirstMatchIn(e.surface).isDefined) &&
-          matchRegex.findFirstIn(e.surface).isEmpty
-    }, pos)
+    val Seq(head, tail@_*) = para
+    val Seq(accHead, accTail@_*) = acc
+    val (newParenStack, isEnding) = rollupParenStack(head, parenStack)
+    val newAccHead = head +: accHead
 
-    val endmark = if (rawEndmark == -1) para.length else rawEndmark
-    val paren = if (rawParen == -1) para.length else rawParen
-
-    if (endmark == paren && paren == para.length) {
-      acc += Sentence(para)
-      acc
-    } else if (open.isEmpty) {
-      if (endmark < paren) {
-        val (sent, next) = para.splitAt(endmark + 1)
-        acc += Sentence(sent)
-        split(next, 0, open, acc)
-      } else {
-        val parenStr = para(paren)
-        val surface = filterRegex.replaceAllIn(parenStr.surface, "")
-        var nOpen = open
-        if (closeParenRegex.findFirstMatchIn(surface).isEmpty) {
-          nOpen ++:= surface.toSeq
-        }
-        split(para, paren + 1, nOpen, acc)
-      }
-    } else {
-      if (paren == para.length) {
-        acc += Sentence(para)
-        acc
-      } else {
-        val parenStr = para(paren)
-        val surface = filterRegex.replaceAllIn(parenStr.surface, "")
-        var nOpen = open
-        if (openParenRegex.findFirstMatchIn(surface).isDefined) {
-          nOpen ++:= surface.toSeq
-        } else if (closeParenRegex.findFirstMatchIn(surface).isDefined) {
-          nOpen = nOpen.tail
-        } else {
-          val top = nOpen.head
-          if (surface.nonEmpty) {
-            if (surface.last == top) nOpen = nOpen.tail
-            else nOpen ++:= surface.toSeq
-          }
-        }
-        split(para, paren + 1, nOpen, acc)
-      }
-    }
+    if (isEnding) split(tail, newParenStack, Seq.empty +: newAccHead +: accTail)
+    else split(tail, newParenStack, newAccHead +: accTail)
   }
 }
